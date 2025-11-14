48f778bf14f19c13e5f27b7e2394d50d01fa1860
sanjay <sanjaygolappanvar1@gmail.com>
2025-11-14 17:03:30 +0530

    Update nodeserver for deployment

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-const io = require('socket.io') (8000,{
-  cors:{
-    origins:"http://127.0.0.1:5500",
+const PORT = process.env.PORT || 8000;
+
+const io = require('socket.io')(PORT, {
+  cors: {
+    origin: "*",
     methods: ["GET", "POST"]
   }
 });
+
+console.log("Socket.io server running on port", PORT);
+
 
 const users = {};
 
